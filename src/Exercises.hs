dcc3d9f88a2a75a22d3e033617dcf1e8e1a40e3a
Zarak <7254237+zarak@users.noreply.github.com>
2020-07-23 14:08:06 +0500

    Fix: descriptions should be normal text

--- src/Exercises.hs
+++ src/Exercises.hs
@@ -4,24 +4,19 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 
 -- |Exercise 1
 -- 
--- @
 -- Change the program so it reads *two* arguments from the command line, and prints out a message using both of them
--- @
 readTwoArgs :: [String] -> String
 readTwoArgs = unwords . take 2
 
 -- |Exercise 2
 -- 
--- @
 -- Change the program so it performs a simple arithmetic operation on the two arguments and prints out the result. 
--- @
 arithmetic :: [String] -> String
 arithmetic = show . sum . map read . take 2
 
 -- |Exercise 3
--- @
--- getLine is an IO action that reads a line from the console and returns it as a string. Change the program so it prompts for a name, reads the name, and then prints that instead of the command line value
--- @
+--
+-- `getLine` is an IO action that reads a line from the console and returns it as a string. Change the program so it prompts for a name, reads the name, and then prints that instead of the command line value
 promptForName :: IO ()
 promptForName = do
     putStrLn "What is your name?"
