f1a7639d1f4651e6157fbcedf9776b80f4028c5d
Zarak <7254237+zarak@users.noreply.github.com>
2020-07-23 18:33:15 +0500

    Make exercise headings bold

--- src/Exercises.hs
+++ src/Exercises.hs
@@ -5,19 +5,19 @@ import Control.Monad
 import Lib
 import Data.Char
 
--- |Exercise 1
+-- |__Exercise 1__
 -- 
 -- Change the program so it reads /two/ arguments from the command line, and prints out a message using both of them.
 readTwoArgs :: [String] -> String
 readTwoArgs = unwords . take 2
 
--- |Exercise 2
+-- |__Exercise 2__
 -- 
 -- Change the program so it performs a simple arithmetic operation on the two arguments and prints out the result. 
 arithmetic :: [String] -> String
 arithmetic = show . sum . map read . take 2
 
--- |Exercise 3
+-- |__Exercise 3__
 --
 -- @getLine@ is an IO action that reads a line from the console and returns it as a string. 
 -- Change the program so it prompts for a name, reads the name, and then prints that instead of the command line value.
@@ -27,25 +27,25 @@ promptForName = do
     name <- getLine
     putStrLn ("Hello, " ++ name)
 
--- |Exercise 4
+-- |__Exercise 4__
 --
 -- Rewrite @parseNumber@, without @liftM@, using
 --
--- 1. @do-notation@
+-- 1. do-notation
 -- 2. explicit sequencing with the @>>=@ operator
 parseNumber :: Parser LispVal
 parseNumber = do
     d <- many1 digit
     return $ Number (read d :: Integer)
 
--- |Exercise 4
+-- |__Exercise 4__
 --
 -- Equivalent version of `parseNumber` using @>>=@.
 parseNumber' :: Parser LispVal
 parseNumber' = many1 digit >>= return . Number . read
 
 
--- |Exercise 5
+-- |__Exercise 5__
 --
 -- Our strings aren't quite R5RS compliant, because they don't support escaping of internal quotes within the string.  
 -- Change 'parseString' so that @\"@ gives a literal quote character instead of terminating the string.  
